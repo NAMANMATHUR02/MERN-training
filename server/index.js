172926d9660f90af4e5317fdea523acedd206383
NAMANMATHUR02 <namanmathur02001@gmail.com>
2021-06-30 14:08:07 +0530

    mern

---
+++ server/index.js
@@ -0,0 +1,10 @@
+// Import Statements
+const express = require('express');
+const app = express();
+const http =require('http');
+
+app.get('/', function(req,res){
+    
+})
+
+app.listen(3000,()=>console.log("running server at 3000"));
